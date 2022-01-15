dfb140e3a4c5d18dde65c2c803cb0d298515b5f4
Julius Robinson <julius.robinson95@yahoo.com>
2022-01-15 15:49:03 -0700

    updated prisma schema and created functions

--- util/prisma.js
+++ util/prisma.js
@@ -1,12 +1,47 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-export const createReimbursement = async (val) => {};
+export const createReimbursement = async (val) => {
+	const reimbursement = await prisma.reimbursement.create({
+		data: {
+			status: val.status,
+			type: val.type,
+			description: val.description,
+			amount: val.amount,
+			author: val.author
+		}
+	});
+};
 
-export const updateReimbursement = async (val) => {};
+export const updateReimbursement = async (val) => {
+	const reimbursement = await prisma.reimbursement.update({
+		where: {
+			id: val.id
+		},
+		data: {
+			status: val.status,
+			reviewer: val.reviewer,
+			note: val.note
+		}
+	});
+};
 
-export const displayAllReimbursements = async (val) => {};
+export const displayAllReimbursements = async (val) => {
+	const reimbursements = await prisma.reimbursement.findMany();
+};
 
-export const displayUsersReimbursements = async (val) => {};
+export const displayUsersReimbursements = async (val) => {
+	const reimbursements = await prisma.reimbursement.findMany({
+		where: {
+			author: val.author
+		}
+	});
+};
 
-export const deleteReimbursement = async (val) => {};
+export const deleteReimbursement = async (val) => {
+	const reimbursement = await prisma.reimbursement.delete({
+		where: {
+			id: val.id
+		}
+	});
+};
